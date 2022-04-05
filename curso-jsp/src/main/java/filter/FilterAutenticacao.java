ed2fdb188af11fc1da2cb2b9c9ccd9ea3780732e
Marlon Cicero Ourives Araujo <marloncicero_araujo@hotmail.com>
2022-04-05 15:07:14 -0400

    Criando a conexão com banco de dados

--- curso-jsp/src/main/java/filter/FilterAutenticacao.java
+++ curso-jsp/src/main/java/filter/FilterAutenticacao.java
@@ -1,7 +1,10 @@
 package filter;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import connection.SingleConnectionBanco;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -16,32 +19,51 @@ import jakarta.servlet.http.HttpSession;
 @WebFilter(urlPatterns = { "/principal/*" })
 public class FilterAutenticacao implements Filter {
 
+	private static Connection connection;
+
 	public FilterAutenticacao() {
 	}
 
 	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		try {
 
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		HttpSession httpSession = httpServletRequest.getSession();
+			HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+			HttpSession httpSession = httpServletRequest.getSession();
 
-		String usuarioLogado = (String) httpSession.getAttribute("usuario");
-		String urlParaAutenticar = httpServletRequest.getServletPath();
-		/* Valida se está logado */
-		if (usuarioLogado == null && !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
-			request.setAttribute("msg", "Por favor realize o login");
-			requestDispatcher.forward(request, response);
-			return;
-		} else {
-			chain.doFilter(request, response);
+			String usuarioLogado = (String) httpSession.getAttribute("usuario");
+			String urlParaAutenticar = httpServletRequest.getServletPath();
+			/* Valida se está logado */
+			if (usuarioLogado == null && !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {
+				RequestDispatcher requestDispatcher = request
+						.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+				request.setAttribute("msg", "Por favor realize o login");
+				requestDispatcher.forward(request, response);
+				return;
+			} else {
+				chain.doFilter(request, response);
+			}
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
+		connection = SingleConnectionBanco.getConnection();
 	}
 
 }
