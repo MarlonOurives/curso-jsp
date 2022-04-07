b12b0e6ecf69dd096728b91d7cd77369151937c6
Marlon Cicero Ourives Araujo <marloncicero_araujo@hotmail.com>
2022-04-07 12:53:57 -0400

    Criando DAOLoginRepository e o método de autenticação com o BD

--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+import dao.DAOLoginRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,9 +11,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-@WebServlet(urlPatterns = {"/ServletLogin", "/principal/ServletLogin"})
+@WebServlet(urlPatterns = { "/ServletLogin", "/principal/ServletLogin" })
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
 	public ServletLogin() {
 	}
@@ -28,36 +31,39 @@ public class ServletLogin extends HttpServlet {
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
 		/* Confere se o login e senha são diferentes de null e não está vazio */
-		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-			/* Cria o objeto e seta os valores */
-			ModelLogin modelLogin = new ModelLogin();
-			modelLogin.setLogin(login);
-			modelLogin.setSenha(senha);
-			/* Simulando um usuário */
-			if (modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin")) {
-				/* Coloca o usuario na sessão */
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());
-				if(url == null || url.equals("null")) {
-					url = "principal/principal.jsp";
+		try {
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+				/* Cria o objeto e seta os valores */
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+				/* Simulando um usuário */
+				if (daoLoginRepository.validarAutenticacao(modelLogin)) {
+					/* Coloca o usuario na sessão */
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+					}
+					/* Redireciona para a tela inicial pós logado */
+					RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
+					requestDispatcher.forward(request, response);
+				} else {
+					/* Faz um redirect para index com mensagem de dados incorretos */
+					RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+					request.setAttribute("msg", "Informe o login e senha corretamente");
+					requestDispatcher.forward(request, response);
 				}
-				/* Redireciona para a tela inicial pós logado */
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
-				requestDispatcher.forward(request, response);
+
 			} else {
-				/* Faz um redirect para index com mensagem de dados incorretos */
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+				/* Cria um dispatcher para redirecionar para a página index.jsp */
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+				/* Seta uma msg para aparecer na tela e chama o forward para enviar tudo */
 				request.setAttribute("msg", "Informe o login e senha corretamente");
 				requestDispatcher.forward(request, response);
 			}
 
-		} else {
-			/* Cria um dispatcher para redirecionar para a página index.jsp */
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
-			/* Seta uma msg para aparecer na tela e chama o forward para enviar tudo */
-			request.setAttribute("msg", "Informe o login e senha corretamente");
-			requestDispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
 	}
-
 }
